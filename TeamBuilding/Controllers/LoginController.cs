baa72b4c9e6c6464928320a2a5836468fd66cf88
k m <k m>
2016-06-12 12:08:20 +0900

    Viewを3つ追加 当プロジェクトに不要なcss,scriptを削除

---
+++ TeamBuilding/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeamBuilding.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        // GET: Login
+        public ActionResult Result()
+        {
+            return View();
+        }
+        // GET: Login
+        public ActionResult ThemeCreate()
+        {
+            return View();
+        }
+    }
+}
