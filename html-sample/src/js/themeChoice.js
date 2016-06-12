cafdbe981206114a11c3aa7ca9d513ca49921c50
J73687 <J73687@jbcc.co.jp>
2016-06-12 14:01:05 +0900

    JSのみの動き部分を完成　参加を一つだけ、新規登録部分の色変更

--- html-sample/src/js/themeChoice.js
+++ html-sample/src/js/themeChoice.js
@@ -2,24 +2,53 @@ $(function () {
   // 新規追加ボタン
   $('.buttonNewPaper').on('click', function () {
     $('.dialogNewPaper').modal()
+    return false
   })
 
-  // ダイアログ表示前にJavaScriptで操作する
+  // ログアウトボタン
+  $('.buttonLogout').on('click', function () {
+    alert('ログアウトしました')
+    return false
+  })
+
+  // 一か所のみ参加を付けるように制御
+  $('.toggle-join').on('change', function(event) {
+    if (event.currentTarget.checked) {
+        // console.log('checked')
+        $('input', $('.toggle-join>.toggle').not('.off')).prop('checked', false).change()
+    }
+    return false
+  })
+
+  // [新規追加ダイアログ] 表示前操作
   $('.dialogNewPaper').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget)
     var recipient = button.data('whatever')
     var modal = $(this)
     modal.find('.modal-body .recipient').text(recipient)
-  // modal.find('.modal-body input').val(recipient)
   })
 
-  // ダイアログ表示直後にフォーカスを設定する
+  // [新規追加ダイアログ] 表示直後操作
   $('.dialogNewPaper').on('shown.bs.modal', function (event) {
+    // フォーカス設定
     $(this).find('.modal-footer .btn-default').focus()
   })
 
-  $('.dialogNewPaper').on('click', '.modal-footer .btn-primary', function () {
+  // [新規追加ダイアログ] 追加ボタン
+  $('.themeAddButton', '.dialogNewPaper').on('click', function () {
     $('.dialogNewPaper').modal('hide')
     alert('テーマを追加しました')
   })
+
+  // [新規追加ダイアログ] 色変更
+  $('li.paperColorSelect').on('click', function(event) {
+    var newColor = $(event.currentTarget).attr('value')
+    var dialogContents = $('div.modal-content', 'div.dialogNewPaper')
+
+    console.log($(event.currentTarget).attr('value'))
+    dialogContents.removeClass('theme-paper-' + dialogContents.attr('value'))
+    dialogContents.attr('value', newColor)
+    dialogContents.addClass('theme-paper-' + newColor)
+  })
+
 })
